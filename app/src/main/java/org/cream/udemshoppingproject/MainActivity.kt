a40b4a0bca2652457ce3b7ffa0561554b31cbe20
Geun Kim <rmsxo5678@naver.com>
2022-08-27 17:35:26 +0900

    first commit

---
+++ app/src/main/java/org/cream/udemshoppingproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.cream.udemshoppingproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activitiy_product_detail)
+    }
+}
