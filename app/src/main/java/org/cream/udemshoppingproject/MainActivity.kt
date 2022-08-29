bc785767add46dd77cc105f7de4742a1730b23e1
Geun Kim <rmsxo5678@naver.com>
2022-08-30 04:18:59 +0900

    하단 탭 구성

--- app/src/main/java/org/cream/udemshoppingproject/MainActivity.kt
+++ app/src/main/java/org/cream/udemshoppingproject/MainActivity.kt
@@ -2,6 +2,7 @@ package org.cream.udemshoppingproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +10,10 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
+        // 아이콘 색상의 틴트를 널로 해서 나타나게 하는 것
+        bottomNavigationView.itemIconTintList = null
 
     }
 }
