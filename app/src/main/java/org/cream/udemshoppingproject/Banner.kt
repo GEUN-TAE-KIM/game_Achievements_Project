f8efc2d1d93084d2a79d95107be9b5eedabc2e92
GEUN-TAE-KIM <rmsxo5678@naver.com>
2022-09-15 12:55:10 +0900

    데이터 할당

--- app/src/main/java/org/cream/udemshoppingproject/Banner.kt
+++ app/src/main/java/org/cream/udemshoppingproject/Banner.kt
@@ -3,13 +3,13 @@ package org.cream.udemshoppingproject
 import com.google.gson.annotations.SerializedName
 
 data class Banner(
-    @SerializedName("background_image_url") val backfroundImageUrl: String,
-    val badge: BannerBadage,
+    @SerializedName("background_image_url") val backgroundImageUrl: String,
+    val badge: BannerBadge,
     val label: String,
     @SerializedName("product_detail") val productDetail: ProductDetail
 )
 
-data class BannerBadage(
+data class BannerBadge(
     val label: String,
     @SerializedName("background_color") val backgroundColor: String
 )
--- app/src/main/java/org/cream/udemshoppingproject/HomeBannerAdapter.kt
+++ app/src/main/java/org/cream/udemshoppingproject/HomeBannerAdapter.kt
@@ -1,5 +1,7 @@
 package org.cream.udemshoppingproject
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +10,9 @@ import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import java.text.DecimalFormat
+import kotlin.math.roundToInt
+
 // ListAdapter 는 레이아웃이 변경됨에따라 해당 데이터를 리스트로 0부터~ 해서 나열해주는 방식인 어뎁터
 class HomeBannerAdapter : ListAdapter<Banner, HomeBannerAdapter.HomeBannerViewHolder>(BannerDiffCallback()) {
 
@@ -28,15 +33,48 @@ class HomeBannerAdapter : ListAdapter<Banner, HomeBannerAdapter.HomeBannerViewHo
         private val bannerTitleTextView = view.findViewById<TextView>(R.id.tv_banner_title)
         private val bannerDetailThumbnailImageView = view.findViewById<ImageView>(R.id.iv_product_detail_thumbnail)
         private val bannerDetailBrandLabelTextView = view.findViewById<TextView>(R.id.tv_product_detail_brand_label)
+        private val bannerDetailProductLabelTextView = view.findViewById<TextView>(R.id.tv_banner_detail_product_label)
         private val bannerDetailDiscountRateTextView = view.findViewById<TextView>(R.id.tv_banner_detail_product_discount_rate)
         private val bannerDetailDiscountPriceTextView = view.findViewById<TextView>(R.id.tv_banner_detail_product_discount_price)
         private val bannerDetailPriceTextView = view.findViewById<TextView>(R.id.tv_banner_detail_product_price)
 
         fun bind(banner: Banner) {
-            GlideApp.with(itemView)
-                .load(banner.backfroundImageUrl)
-                .into(bannerImageView)
+
+            loadImage(banner.backgroundImageUrl,bannerImageView)
+
+            bannerBadgeTextView.text = banner.badge.label
+            bannerBadgeTextView.background = ColorDrawable(Color.parseColor(banner.badge.backgroundColor))
+            bannerTitleTextView.text = banner.label
+            loadImage(banner.productDetail.thumbnail_image_url,bannerDetailThumbnailImageView)
+            bannerDetailBrandLabelTextView.text = banner.productDetail.brandName
+            bannerDetailProductLabelTextView.text = banner.productDetail.label
+            bannerDetailDiscountRateTextView.text = "${banner.productDetail.discountRate}%"
+            calculateDiscountAmount(bannerDetailDiscountPriceTextView, banner.productDetail.discountRate, banner.productDetail.price)
+            applyPriceFormat(bannerDetailPriceTextView, banner.productDetail.price)
+
         }
+
+        // 할인 함수
+        private fun calculateDiscountAmount(view: TextView, discountRate: Int, price: Int) {
+            val discountPrice = (((100 - discountRate) / 100.0) * price).roundToInt()
+            applyPriceFormat(view,discountPrice)
+        }
+
+        // 호출 숫자, 및 단위 표시들
+        private fun applyPriceFormat(view: TextView, price: Int) {
+            val decimalFormat = DecimalFormat("#,###")
+            view.text = decimalFormat.format(price) + "원"
+        }
+
+        private fun loadImage(urlString: String, imageView: ImageView) {
+
+            GlideApp.with(itemView)
+                .load(urlString)
+                .into(imageView)
+
+        }
+
+
     }
 
 }
