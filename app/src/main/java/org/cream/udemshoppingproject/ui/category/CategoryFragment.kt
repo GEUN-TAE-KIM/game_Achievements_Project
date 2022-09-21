e3c1371ffb1b78e5b8f72c572d677dc7a3af4099
GEUN-TAE-KIM <rmsxo5678@naver.com>
2022-09-21 18:21:12 +0900

    data binding or adapter

--- app/src/main/java/org/cream/udemshoppingproject/ui/category/CategoryFragment.kt
+++ app/src/main/java/org/cream/udemshoppingproject/ui/category/CategoryFragment.kt
@@ -8,25 +8,31 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import org.cream.udemshoppingproject.R
+import org.cream.udemshoppingproject.databinding.FragmentCartegoryBinding
 import org.cream.udemshoppingproject.ui.common.ViewModelFactory
 
 class CategoryFragment: Fragment() {
 
     private val viewModel: CategoryViewModel by viewModels { ViewModelFactory(requireContext())}
+    private lateinit var binding: FragmentCartegoryBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_cartegory,container,false)
+        binding = FragmentCartegoryBinding.inflate(inflater,container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val categoryAdapter = CategoryAdapter()
+        binding.rvCategoryList.adapter = categoryAdapter
         viewModel.items.observe(viewLifecycleOwner) {
-            Log.d("CategoryFragment","items=$it")
+            categoryAdapter.submitList(it)
+            //Log.d("CategoryFragment","items=$it")
         }
     }
 }
