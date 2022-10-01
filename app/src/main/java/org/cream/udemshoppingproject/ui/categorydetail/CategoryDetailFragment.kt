0155eefdfd8ba032b79ea3e9787120e1a5382480
GEUN-TAE-KIM <rmsxo5678@naver.com>
2022-10-02 01:37:40 +0900

    ConcatAdapter 어댑터를 하나로 연결

--- app/src/main/java/org/cream/udemshoppingproject/ui/categorydetail/CategoryDetailFragment.kt
+++ app/src/main/java/org/cream/udemshoppingproject/ui/categorydetail/CategoryDetailFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ConcatAdapter
 import org.cream.udemshoppingproject.common.KEY_CATEGORY_ID
 import org.cream.udemshoppingproject.common.KEY_CATEGORY_LABEL
 import org.cream.udemshoppingproject.databinding.FragmentCategoryDetailBinding
@@ -27,8 +28,22 @@ class CategoryDetailFragment: Fragment() {
 
         binding.lifecycleOwner = viewLifecycleOwner
 
-       val categoryId = requireArguments().getString(KEY_CATEGORY_ID)
-       val categoryLabel = requireArguments().getString(KEY_CATEGORY_LABEL)
+       setToolbar()
+       setListAdapter()
+
+    }
+
+    private fun setToolbar() {
+        val categoryLabel = requireArguments().getString(KEY_CATEGORY_LABEL)
         binding.toolbarCategoryDetail.title = categoryLabel
     }
+
+    private fun setListAdapter() {
+
+        val titleAdapter = CategorySectionTitleAdapter()
+        val promotionAdapter = CategoryPromotionAdapter()
+                                            // 어댑터를 하나로 만드는 것
+        binding.rvCategoryDetail.adapter = ConcatAdapter(titleAdapter, promotionAdapter)
+    }
+
 }
